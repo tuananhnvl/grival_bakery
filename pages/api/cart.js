9b4e4737b3d4ace53a27fbf39ed399f6e0459dc1
tuananhnvl <tuananh.namvietluat@gmail.com>
2023-07-16 21:05:43 +0700

    up

--- pages/api/cart.js
+++
@@ -1,8 +0,0 @@
-
-import {Product} from "@/models/Product";
-
-export default async function handle(req,res) {
-  await mongooseConnect();
-  const ids = req.body.ids;
-  res.json(await Product.find({_id:ids}));
-}
