a983e5834aacf79f8bd4a1744d1a4b0ef9aeb9c7
tuananhnvl <tuananh.namvietluat@gmail.com>
2023-07-26 18:31:17 +0700

    ypp

--- pages/_document.js
+++ pages/_document.js
@@ -31,22 +31,20 @@ export default class MyDocument extends Document {
     return (
       <Html lang="vi">
         <Head>
-          {/* <Script strategy="afterInteractive" dangerouslySetInnerHTML={{
+          <Script strategy="afterInteractive" dangerouslySetInnerHTML={{
             __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
 new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
 j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
 'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-})(window,document,'script','dataLayer','GTM-MP8P77CC');`}}></Script> */}
+})(window,document,'script','dataLayer','GTM-MP8P77CC');`}}></Script>
         </Head>
         <body>
-        {/* <noscript>
-          <iframe
-            src={`https://www.googletagmanager.com/ns.html?id=GTM-MP8P77CC`}
-            height="0"
-            width="0"
-            style={{ display: "none", visibility: "hidden" }}
-          />
-        </noscript> */}
+
+
+<noscript dangerouslySetInnerHTML={{ __html: `<iframe src="https://www.googletagmanager.com/ns.html?id=GTM-XXXXX"
+            height="0" width="0" style="display:none;visibility:hidden"></iframe>`}}></noscript>
+
+
           <Main />
           <NextScript />
           <div id="modal-root-protal"></div>
