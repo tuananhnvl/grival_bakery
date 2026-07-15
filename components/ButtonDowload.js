345a9961309b6a78c31b92f6766d12256718d6f6
brandyvt95 <brandy.hontran.dev@gmail.com>
2026-07-15 10:49:08 +0700

    d

--- components/ButtonDowload.js
+++ components/ButtonDowload.js
@@ -4,11 +4,11 @@ export default function ButtonDowload({children,type}) {
 function download(type) {
     if(type == 'baogia') {
         if (typeof window !== "undefined"){
-            window.location.href = `${process.env.NEXT_PUBLIC_DOMAIN_HOST}/asset_2025/Bang_gia_brodard_2025.pdf`
+            window.location.href = `${process.env.NEXT_PUBLIC_DOMAIN_HOST}/asset_2026/Banggia_brodard_0785777768.pdf`
         }
     }else if(type == 'banggia') {
         if (typeof window !== "undefined"){
-            window.location.href = `${process.env.NEXT_PUBLIC_DOMAIN_HOST}/asset_2025/Bang_gia_brodard_2025.pdf`
+            window.location.href = `${process.env.NEXT_PUBLIC_DOMAIN_HOST}/asset_2026/Banggia_brodard_0785777768.pdf`
         }
     }
 }
